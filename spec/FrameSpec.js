c78fdbd2e9b379489f5a5188942726956e696c66
Prabu Sathan <prabu.sathan@gmail.com>
2017-07-27 23:52:41 +0100

    A frame can be created and starts as an array containing [null,null]

---
+++ spec/FrameSpec.js
@@ -0,0 +1,15 @@
+describe('Frame', function() {
+  var frame;
+
+  beforeEach(function() {
+    frame = new Frame;
+  });
+
+  describe('bowls', function() {
+
+    it('starts out as an array containing null, null', function() {
+      expect(frame.bowls()).toEqual([null, null])
+    });
+
+  });
+});
---
+++ src/Frame.js
@@ -0,0 +1,7 @@
+var Frame = function() {
+  this._bowls = [null, null]
+}
+
+Frame.prototype.bowls = function() {
+  return this._bowls
+}
